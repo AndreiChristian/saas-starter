c52cd39f58c2956928c75d70285160301b14379b
Andrei <andreichristiannetoiu@gmail.com>
2024-12-14 13:35:05 +0200

    x

--- src/layouts/UnauthenticatedLayout.tsx
+++ src/layouts/UnauthenticatedLayout.tsx
@@ -1,22 +1,12 @@
-import { Outlet } from "react-router-dom";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "../components/ui/navigation-menu";
+import { Link, Outlet } from "react-router-dom";
 
 export default function UnathenticatedLayout() {
   return <section className="h-screen max-h-screen box-border flex flex-col items-center justify-start" >
-    <nav className="w-full bg-bg box-border border-border border p-5 " >
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <nav className="w-full bg-bg box-border p-5 flex  gap-5" >
+      <Link to={'/'} >SaasStarter</Link>
+      <span className="flex-1"></span>
+      <Link to={'/login'} >Login</Link>
+      <Link to={'/signup'} >Signup</Link>
     </nav>
     <Outlet />
   </section>
