d8c65ecdfb1ababbf2b26d25b605545937883b77
Andrei <andreichristiannetoiu@gmail.com>
2024-12-14 13:18:10 +0200

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
@@ -121,6 +122,6 @@ export default {
       }
     }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [import("tailwindcss-animate")],
 }
 
